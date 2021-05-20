1932ae4f1d45bbfce63d50324e9f718d9ae96dce
rbudai <robert.mihai.budai@accenture.com>
2021-05-20 10:06:01 +0300

    Data base connection

--- Chat/Login.cs
+++ Chat/Login.cs
@@ -7,14 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
 namespace Chat
 {
     public partial class Login : Form
     {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "inXj8sTPJUWlgLwQVJZSR2p4NR7EeJ4xZwHobH09",
+            BasePath = "https://poli-chat-db-default-rtdb.firebaseio.com/"
+        };
+        IFirebaseClient client;
         public Login()
         {
             InitializeComponent();
+            client = new FireSharp.FirebaseClient(config);
+            if (client != null) 
+                MessageBox.Show("conn established"); 
+            else 
+                MessageBox.Show("conn err"); 
         }
 
         private void button1_Click(object sender, EventArgs e)
