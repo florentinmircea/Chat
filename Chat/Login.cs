0e7531d14579595da175782a9fd898e766c25c4a
rbudai <robert.mihai.budai@accenture.com>
2021-05-20 10:55:44 +0300

    still not working

--- Chat/Login.cs
+++ Chat/Login.cs
@@ -7,9 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Castle.MicroKernel.Registration;
 using FireSharp.Config;
 using FireSharp.Interfaces;
 using FireSharp.Response;
+using Microsoft.Graph;
+
 namespace Chat
 {
     public partial class Login : Form
@@ -24,14 +27,31 @@ namespace Chat
         {
             InitializeComponent();
             client = new FireSharp.FirebaseClient(config);
-            if (client != null) 
-                MessageBox.Show("conn established"); 
-            else 
-                MessageBox.Show("conn err"); 
+            if (client != null) { }
+            //MessageBox.Show("conn established"); 
+            else
+                MessageBox.Show("Connection error"); 
         }
-
+        private string users = "";
+        async private void get_users()
+        {
+            //Task response = await client.Get(@"poli-chat-db-default-rtdb/users");
+            //string ResUser = response.ResultAs<string>();// database result
+            
+        }
         private void button1_Click(object sender, EventArgs e)
         {
+            string current_username = textBox1.Text;
+            string current_password = textBox2.Text;
+            if (current_password == "" || current_username == "")
+            {
+                MessageBox.Show("Please insert Username and password!");
+            }
+            else
+            {
+                //get_users();
+                //MessageBox.Show(users);
+            }
             this.Hide();
             MainForm mainForm = new MainForm(this);
             mainForm.ShowDialog();
